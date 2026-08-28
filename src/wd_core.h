ce82a2b17db124a5a3907d1a1e2b325bfd10beef
Sergioba99 <64738043+Sergioba99@users.noreply.github.com>
2026-08-28 20:47:32 +0200

    Align core interface with extracted implementation

--- src/wd_core.h
+++ src/wd_core.h
@@ -1,8 +1,6 @@
 #ifndef WD_FORCE_STEREO_CORE_H
 #define WD_FORCE_STEREO_CORE_H
 
-typedef unsigned long DWORD;
-typedef int BOOL;
 typedef void *HINSTANCE;
 
 void WDCoreProcessAttach(HINSTANCE module);
