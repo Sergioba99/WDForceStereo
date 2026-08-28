ab650acb188c19951e94e38a42a2186457341b2d
Sergioba99 <64738043+Sergioba99@users.noreply.github.com>
2026-08-28 20:25:15 +0200

    Add shared WDForceStereo core interface

---
+++ src/wd_core.h
@@ -0,0 +1,10 @@
+#ifndef WD_FORCE_STEREO_CORE_H
+#define WD_FORCE_STEREO_CORE_H
+
+typedef unsigned long DWORD;
+typedef int BOOL;
+typedef void *HINSTANCE;
+
+void WDCoreProcessAttach(HINSTANCE module);
+
+#endif
