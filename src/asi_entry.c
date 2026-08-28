dd14810e28b1c740d39f8927da649e40445222dc
Sergioba99 <64738043+Sergioba99@users.noreply.github.com>
2026-08-28 22:03:00 +0200

    Use shared core header in ASI entry

--- src/asi_entry.c
+++ src/asi_entry.c
@@ -3,20 +3,19 @@
 // This facade contains no proxy exports. An ASI loader only needs to load the
 // module; DLL_PROCESS_ATTACH starts the shared WDForceStereo core.
 
+#include "wd_core.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef unsigned long DWORD;
 typedef int BOOL;
-typedef void *HINSTANCE;
 typedef void *LPVOID;
 
 #define WINAPI __stdcall
 #define TRUE 1
 #define DLL_PROCESS_ATTACH 1
-
-void WDCoreProcessAttach(HINSTANCE module);
 
 BOOL WINAPI DllMain(HINSTANCE h, DWORD reason, LPVOID reserved) {
   (void)reserved;
